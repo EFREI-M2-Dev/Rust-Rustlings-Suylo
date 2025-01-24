604cf5c908478139d7d7b162d595004bc710086d
Suylo <spoownz@yahoo.com>
2025-01-24 10:35:53 +0100

    feat: IF soluces

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -1,9 +1,12 @@
 // TODO: Fix the compiler error on this function.
 fn picky_eater(food: &str) -> &str {
+    // SOLUTION: Utiliser else if et else
     if food == "strawberry" {
         "Yummy!"
+    } else if food == "potato" {
+        "I guess I can eat that."
     } else {
-        1
+        "No thanks!"
     }
 }
 
